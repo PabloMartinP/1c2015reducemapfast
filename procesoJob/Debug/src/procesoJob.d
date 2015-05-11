f9d8fb3b10da32e7725e5b9b03ae862f7ba477a1
yamisanabria <yamiisanabriia@gmail.com>
2015-05-11 18:25:55 -0300

    creado el proyecto job

---
+++ procesoJob/Debug/src/procesoJob.d
@@ -0,0 +1,1 @@
+src/procesoJob.d: ../src/procesoJob.c
---
+++ procesoJob/src/procesoJob.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : procesoJob.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/collections/list.h>
+#include <pthread.h>
+
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}

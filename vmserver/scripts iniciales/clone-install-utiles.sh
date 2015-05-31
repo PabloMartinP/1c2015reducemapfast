b531d2e347ebd16de23eae3a611b51d5c9f5bcaf
Pablo Martin <pablo.penialoza@gmail.com>
2015-05-31 16:03:13 -0300

    borrado script iniciales

--- vmserver/scripts iniciales/clone-install-utiles.sh
+++ vmserver/scripts iniciales/clone-install-utiles.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # My first script
 
-mkdir /home/utnso/Escritorio
-mkdir /home/utnso/Escritorio/git
-cd /home/utnso/Escritorio/git
-git clone https://github.com/sisoputnfrba/tp-2015-1c-dalemartadale.git /home/utnso/Escritorio/git
-
 cd /home/utnso/Escritorio/git/tp-2015-1c-dalemartadale/vmserver/utiles/Debug
 make
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/Escritorio/git/tp-2015-1c-dalemartadale/utiles/Debug
 
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/Escritorio/git/tp-2015-1c-dalemartadale/utiles/Debug
+echo "Export library path creado OK"
 

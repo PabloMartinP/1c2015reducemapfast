381f086a9bf27feda836da7114c04f85e3b29850
PabloMartinP <pablo.penialoza@gmail.com>
2015-07-20 13:52:57 -0300

    fix export

--- vmserver/scripts iniciales/instalar-procesoMaRTA.sh
+++ vmserver/scripts iniciales/instalar-procesoMaRTA.sh
@@ -7,7 +7,7 @@ cd /home/utnso/Escritorio/git/tp-2015-1c-dalemartadale/procesoMaRTA/Debug
 make clean
 make
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/Escritorio/git/tp-2015-1c-dalemartadale/utiles/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH=/home/utnso/Escritorio/git/tp-2015-1c-dalemartadale/utiles/Debug
 
 echo "Export library path creado OK"
 echo "MaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTAMaRTA"
